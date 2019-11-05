553f21a44f91a32b83e6415275bb7c0f9e581cce
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-05 01:58:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,24 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+  i = 0
+  min = 0
+  min_key = nil
+  hash.each do |key, value|
+    unless i > 0
+      min = value
+      min_key = key
+    end
+    if min > value
+      min = value
+      min_key = key
+    end
+    i += 1
+  end
+  min_key
 end
+
+key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})
+
+key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})
